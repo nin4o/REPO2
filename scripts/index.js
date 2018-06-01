0683c6cb476d9a63b240e0fd7842ad4c4869979a
NAIDEN NAIDENOV <nin4o_mn@abv.bg>
2018-06-01 14:38:33 +0300

    JS shaping

---
+++ scripts/index.js
@@ -0,0 +1,50 @@
+function vudici() {
+    document.getElementById("test").innerHTML="Очаквайте скоро въдици!";
+    document.getElementById("test").style.color="#990099";
+}
+
+function makari() {
+    document.getElementById("test").innerHTML="Очаквайте скоро макари!";
+    document.getElementById("test").style.color="#990099";
+}
+
+function vlakna() {
+    document.getElementById("test").innerHTML="Очаквайте скоро влакна!";
+    document.getElementById("test").style.color="#990099";
+}
+
+function pluvki() {
+    document.getElementById("test").innerHTML="Очаквайте скоро плувки!";
+    document.getElementById("test").style.color="#990099";
+}
+
+function kuki() {
+    document.getElementById("test").innerHTML="Очаквайте скоро куки!";
+    document.getElementById("test").style.color="#990099";
+}
+
+function fiderRibolov() {
+    document.getElementById("test").innerHTML="Очаквайте скоро Фидер риболов!";
+    document.getElementById("test").style.color="#990099";
+}
+
+function ribolovNaSharan() {
+    document.getElementById("test").innerHTML="Очаквайте скоро Риболов на шаран!";
+    document.getElementById("test").style.color="#990099";
+}
+
+function spiningRibolov() {
+    document.getElementById("test").innerHTML="Очаквайте скоро Спининг риболов!";
+    document.getElementById("test").style.color="#990099";
+}
+
+function morskiRibolov() {
+    document.getElementById("test").innerHTML="Очаквайте скоро Морски риболов!";
+    document.getElementById("test").style.color="#990099";
+}
+
+function ribolovNaPluvka() {
+    document.getElementById("test").innerHTML="Очаквайте скоро Риболов на плувка!";
+    document.getElementById("test").style.color="#990099";
+}
+
