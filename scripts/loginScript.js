54bec4b0e898f8baeb7260071d7d27daebac4f26
NAIDEN NAIDENOV <nin4o_mn@abv.bg>
2018-06-01 19:07:59 +0300

    login done

--- scripts/loginScript.js
+++ scripts/loginScript.js
@@ -3,7 +3,7 @@ firebase.auth().onAuthStateChanged(function(user) {
       // User is signed in.
   
       document.getElementById("user_div").style.display = "block";
-      document.getElementById("login_div").style.display = "none";
+      document.getElementById("testDiv").style.display = "none";
   
       var user = firebase.auth().currentUser;
   
@@ -11,6 +11,7 @@ firebase.auth().onAuthStateChanged(function(user) {
   
         var email_id = user.email;
         document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+        console.log(user.email);
   
       }
   
@@ -18,7 +19,7 @@ firebase.auth().onAuthStateChanged(function(user) {
       // No user is signed in.
   
       document.getElementById("user_div").style.display = "none";
-      document.getElementById("login_div").style.display = "block";
+      document.getElementById("testDiv").style.display = "block";
   
     }
   });
